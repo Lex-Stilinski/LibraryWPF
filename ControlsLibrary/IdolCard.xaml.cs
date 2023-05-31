b4d40af2067c00f9c5c9527611010ab3f4fdaf6f
Lex-Stilinski <t50_ya.v.komarkova@mpt.ru>
2023-05-31 21:40:16 +0300

    Практическая №8

---
+++ ControlsLibrary/IdolCard.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ControlsLibrary
+{
+    /// <summary>
+    /// Логика взаимодействия для IdolCard.xaml
+    /// </summary>
+    public partial class IdolCard : UserControl
+    {
+        public IdolCard()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ De_SerializationLibrary/De_Serialization.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System.Xml.Serialization;
+
+namespace De_SerializationLibrary
+{
+    public class DataSerialization
+    {
+        public static T DeserializeXml<T>(string xmlData)
+        {
+            var serializer = new XmlSerializer(typeof(T));
+            using (TextReader reader = new StringReader(xmlData))
+            {
+                return (T)serializer.Deserialize(reader);
+            }
+        }
+
+        public static T DeserializeJson<T>(string jsonData)
+        {
+            return JsonConvert.DeserializeObject<T>(jsonData);
+        }
+
+        public static string SerializeXml<T>(T obj)
+        {
+            var serializer = new XmlSerializer(typeof(T));
+            using (StringWriter writer = new StringWriter())
+            {
+                serializer.Serialize(writer, obj);
+                return writer.ToString();
+            }
+        }
+
+        public static string SerializeJson<T>(T obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+    }
+}
